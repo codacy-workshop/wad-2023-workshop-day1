db33d0db9d6285f61aae92b0aa6072e4bed97eb6
soltan <soltangh.work@gmail.com>
2023-07-27 16:21:53 +0200

    Fix 09.js

--- 09.js
+++ 09.js
@@ -1,9 +1,14 @@
+function main(){
+    let x = 4;
+    const isThatTrue = false;
 
-var x = 4
-var isThatTrue = false
-
-if ((x = 12) && isThatTrue) {
-    console.log("Javascript is the best!")
+    x = 12;
+    if ((x === 12) && isThatTrue) {
+        console.log("Javascript is the best!")
+    }
+    console.log(x)
 }
 
-console.log(x)
+
+
+
