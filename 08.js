c2338805dd6c326fec1cf6309a75b182cf0b3e5a
Anton Ponomarenko <anton.ponomarenko@gmx.de>
2023-07-27 16:27:12 +0200

    new changes

--- 08.js
+++ 08.js
@@ -1,6 +1,6 @@
 function calculateArea(radius) {
-    var pi = 3.141592653589793238462643383279502884197;
-    var area = pi * radius * radius;
+    let pi = 3.141592653589793238462643383279502884197;
+    let area = pi * radius * radius;
     return area;
 }
 
