4de93ab49117688b706b20e6595c83deccfd40aa
soltan <soltangh.work@gmail.com>
2023-07-27 16:23:54 +0200

    Fix 12.js

--- 12.js
+++ 12.js
@@ -5,7 +5,9 @@ function bubbleSort(arr) {
     do {
       swapped = false;
       for (let i = 0; i < len - 1; i++) {
-        if ((arr[i] > arr[i + 1]) && (temp = arr[i])) {
+        let temp;
+        if ((arr[i] > arr[i + 1])) {
+        temp = arr[i];
           arr[i] = arr[i + 1];
           arr[i + 1] = temp;
           swapped = true;
