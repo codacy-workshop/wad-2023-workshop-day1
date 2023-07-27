32df526097764da7a97a02570852dc2c7d5fa975
soltan <soltangh.work@gmail.com>
2023-07-27 16:39:47 +0200

    fix 04.js

--- 04.js
+++ 04.js
@@ -1,5 +1,13 @@
 function createNumberFunctions() {
-    var nums = []; for (var i = 0; i < 10; i++) { (function (i) { nums[i] = function (j) { return i + j; }; }(i)); } return nums;
+    var nums = [];
+    for (let i = 0; i < 10; i++) {
+        (function(i) {
+            nums[i] = function(j) {
+                return i + j;
+            };
+        })(i);
+    }
+    return nums;
 }
 
 const numberFunctions = createNumberFunctions();
