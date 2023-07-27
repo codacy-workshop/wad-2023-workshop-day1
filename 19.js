602c84b6f02ae199589a242a41a89fab859b6801
Andi Sejdiu <a.sejdiu@celonis.com>
2023-07-27 16:40:29 +0200

    changes

--- 19.js
+++ 19.js
@@ -6,27 +6,21 @@ function determineWeatherCondition(weatherData) {
             } else {
                 return "Hot and humid with strong wind.";
             }
+        } else if (weatherData.windSpeed < 10) {
+            return "Hot and dry with light breeze.";
         } else {
-            if (weatherData.windSpeed < 10) {
-                return "Hot and dry with light breeze.";
-            } else {
-                return "Hot and dry with strong wind.";
-            }
+            return "Hot and dry with strong wind.";
         }
+    } else if (weatherData.humidity > 80) {
+        if (weatherData.windSpeed < 10) {
+            return "Cool and humid with light breeze.";
+        } else {
+            return "Cool and humid with strong wind.";
+        }
+    } else if (weatherData.windSpeed < 10) {
+        return "Cool and dry with light breeze.";
     } else {
-        if (weatherData.humidity > 80) {
-            if (weatherData.windSpeed < 10) {
-                return "Cool and humid with light breeze.";
-            } else {
-                return "Cool and humid with strong wind.";
-            }
-        } else {
-            if (weatherData.windSpeed < 10) {
-                return "Cool and dry with light breeze.";
-            } else {
-                return "Cool and dry with strong wind.";
-            }
-        }
+        return "Cool and dry with strong wind.";
     }
 }
 
