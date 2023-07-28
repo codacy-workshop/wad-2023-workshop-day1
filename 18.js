fb8588900c3fc0b49f0ae8b6b5b8eda423d5f6fe
Justin Braun <justin.braun@timeflex.de>
2023-07-28 14:10:12 +0200

    fix?

--- 18.js
+++ 18.js
@@ -1,10 +1,11 @@
+function logThingy() {
+    console.log("Function " + i + " was called.");
+}
 function generateFunctions() {
     var functions = [];
 
     for (var i = 0; i < 3; i++) {
-        functions.push(() => {
-            console.log("Function " + i + " was called.");
-        });
+        functions.push(logThingy.bind(i));
     }
 
     return functions;
