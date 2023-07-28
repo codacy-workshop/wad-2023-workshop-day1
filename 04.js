cffda80bf4fac0a3c1a72c23302a76d303bc26fa
flix-code <139325744+flix-code@users.noreply.github.com>
2023-07-28 13:53:12 +0200

    Fix obvious issues for 04.js - function issue remains

--- 04.js
+++ 04.js
@@ -1,14 +1,11 @@
-var nums = [];
+let nums = [];
 
 function createNumberFunctions() { 
-    var temp;
-
     for (i = 0; i < 10; i++) {
         nums[i] = function (j) {
             return i + j;
         };
     }
-
     return nums;
 }
 
