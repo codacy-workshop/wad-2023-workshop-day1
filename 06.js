78ba171017452d03b3cd3b0a7ee2368824f6931a
flix-code <139325744+flix-code@users.noreply.github.com>
2023-07-28 13:59:53 +0200

    Fix 06.js

--- 06.js
+++ 06.js
@@ -1,7 +1,7 @@
 function sumArray(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == 0) {
+        if (!isNaN(arr[i]) && arr[i] !== 0) {
             sum += arr[i];
         }
     }
