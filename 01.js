e4a0b6369392968ad17281b70b40e9b3bb06786d
Lukas Corona <lukas.corona@t-systems.com>
2023-07-28 13:45:41 +0200

    refactor

--- 01.js
+++ 01.js
@@ -2,5 +2,5 @@ for (var i = 1; i <= 100; i++) {
   let message = "";
   if (i % 3 === 0) message += "Fizz";
   if (i % 5 === 0) message += "Buzz";
-  return message || i;
+  console.log(message || i);
 }
