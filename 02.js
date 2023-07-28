abba73cd567f2df9874e00e64624ccb695a9f76f
Johan Baltié <jbaltie@abbelight.com>
2023-07-28 13:41:51 +0200

    fixup! Fix 02.js

--- 02.js
+++ 02.js
@@ -1,23 +1,15 @@
 function getSeason(month) {
     switch (month) {
-        case 1:
-        case 2:
-        case 12:
+        case 1,2,12:
             console.log("It's winter.");
             break;
-        case 3:
-        case 4:
-        case 5:
+        case 3,4,5:
             console.log("It's spring.");
             break;
-        case 6:
-        case 7:
-        case 8:
+        case 6,7,8:
             console.log("It's summer.");
             break;
-        case 9:
-        case 10:
-        case 11:
+        case 9,10,11:
             console.log("It's autumn.");
             break;
         default:
