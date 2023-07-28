ae2e19c4fcff6f1219756542475f79c9dc97261f
Alannah David <alannah@welldhealth.com>
2023-07-28 14:12:21 +0200

    f

--- 19.js
+++ 19.js
@@ -2,25 +2,23 @@ function determineWeatherCondition(weatherData) {
     if (weatherData.temperature > 30) {
         if (weatherData.humidity > 80) {
             if (weatherData.windSpeed < 10) {
-                return "Hot and humid with light breeze.";
-            } else {
-                return "Hot and humid with strong wind.";
+              return "Hot and humid with light breeze.";
             }
+            return "Hot and humid with strong wind.";
         } else {
             if (weatherData.windSpeed < 10) {
-                return "Hot and dry with light breeze.";
-            } else {
-                return "Hot and dry with strong wind.";
-            }
+              return "Hot and dry with light breeze.";
+            } 
+            return "Hot and dry with strong wind.";
         }
     } else {
         if (weatherData.humidity > 80) {
             if (weatherData.windSpeed < 10) {
-                return "Cool and humid with light breeze.";
+              return "Cool and humid with light breeze.";
             }
         } else {
             if (weatherData.windSpeed < 10) {
-                return "Cool and dry with light breeze.";
+              return "Cool and dry with light breeze.";
             }
         }
     }
