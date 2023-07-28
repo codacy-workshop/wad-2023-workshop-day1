47df1c35173efcfbe3c7ecf680143d0882f5f376
Johan Baltié <jbaltie@abbelight.com>
2023-07-28 14:06:49 +0200

    fixup! fixup! fixup! Fix 04.js

--- 04.js
+++ 04.js
@@ -3,7 +3,7 @@ function createNumberFunctions() {
     var nums = [];
 
     for (var i = 0; i < 10; i++) {
-        function (i) { nums[i] = function (j) { return i + j; }; }(i);
+        (function (i) { nums[i] = function (j) { return i + j; }; })(i);
     }
 
     return nums;
