7f97cd735f607544a240f1ad91632baf20ae259a
Lukas Corona <lukas.corona@t-systems.com>
2023-07-28 13:35:38 +0200

    refactor: 01

--- 01.js
+++ 01.js
@@ -1,11 +1,6 @@
 for (var i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 == 0) {
-    console.log("Fizz");
-  } else if (i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
+  let message = "";
+  if (i % 3 === 0) message += "Fizz";
+  if (i % 5 === 0) message += "Buzz";
+  return message || i;
 }
