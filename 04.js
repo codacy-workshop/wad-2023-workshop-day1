5f707db11fe8312211dfe3d587225aeddfa47542
Justin Braun <justin.braun@timeflex.de>
2023-07-28 13:35:32 +0200

    fix 4

--- 04.js
+++ 04.js
@@ -1,8 +1,6 @@
-var nums = [];
 
-function createNumberFunctions() { 
-    var temp;
-
+function createNumberFunctions() {
+    var nums = [];
     for (i = 0; i < 10; i++) {
         nums[i] = function (j) {
             return i + j;
