bbfc1acfd4ecd498947b6d49db23f73ecd8431ac
Rico Neitzel <rico@run-as-root.sh>
2023-07-29 21:24:32 +0200

    fixes 16

--- 16.js
+++ 16.js
@@ -1,14 +1,18 @@
-var globalSum = 0;
-
-var elem = 2
+const factor = 2
 
 function calculateSum(obj) {
+  let globalSum = 0;
+  let elem;
+
   for (elem in obj) {
-    globalSum += elem;
+    globalSum += obj[elem];
   }
+
+  return globalSum;
 }
 
-var data = [10, 20, 30];
+const data = [10, 20, 30];
 
-calculateSum(data);
-console.log(globalSum * elem);
+const calculatedSum = calculateSum(data);
+console.log(calculatedSum);
+console.log(calculatedSum * factor);
